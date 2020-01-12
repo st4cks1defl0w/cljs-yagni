9ad0257108aaf734b0c1ca0951269be2f92d1e01
stacksideflow <1@stacksideflow.host>
2020-01-13 01:33:06 +0300

    Adjust project.clj naming

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cljs-toolbox "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject cljs-yagni "0.9"
+  :description "Cljs-toolbox short-lived repl yagni tool"
+  :url "https://stacksideflow.host"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
