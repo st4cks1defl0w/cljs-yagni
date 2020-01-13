e0bf9218dee282305eada5477f19784d99dbff8d
stacksideflow <1@stacksideflow.host>
2020-01-13 04:04:55 +0300

    Cleanup unused files and dirs

--- test/cljs_toolbox/cljs_toolbox_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cljs-toolbox.cljs-toolbox-test
-  (:require [clojure.test :refer :all]
-            [cljs-toolbox.cljs-toolbox :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
